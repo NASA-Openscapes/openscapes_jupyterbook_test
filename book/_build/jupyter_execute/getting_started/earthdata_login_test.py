3e5c8d604e48d40d0c3bf432f2a59429a7fd53dd
jules32 <lowndes@nceas.ucsb.edu>
2021-06-01 15:48:06 -0700

    rebuilding book with TOC disabling in _config.yml

---
+++ book/_build/jupyter_execute/getting_started/earthdata_login_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ---
+# # An .ipynb stub for data passwords <a id="getstarted"></a>
+
+# See Christine's post & conversation on the Jupyter discourse forum:  <https://discourse.jupyter.org/t/how-do-i-properly-protect-my-data-access-passwords-not-jupyter-tokens-passwords-on-3rd-party-jupyter-hub-services/9277>
+
+# ## This is otherwise just testing <a id="1.1"></a>
+# #### Import the required packages and set the input/working directory to run this Jupyter Notebook locally.
+
+# In[1]:
+
+
+import requests as r
+from skimage import io
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+print(1+1)
+print("woo NASA data!")
+
